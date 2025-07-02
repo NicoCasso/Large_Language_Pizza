cd291923b21fe12e39d3eb6a5f35bc3ce40aa3e5
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-07-02 09:50:26 +0200

    first chat response

--- minimal_interface.py
+++ minimal_interface.py
@@ -1,16 +1,19 @@
 import gradio as gr
 
+from camel_wrapper import LamaWrapper
+
+lama = LamaWrapper()
+
 def respond(message, history):
-    # Ici, vous pouvez ajouter la logique de votre chatbot
-    return "Écho: " + message
+    lama_answer = lama.rag_chain.invoke(message)
+    return lama_answer 
 
 # Création de l'interface Gradio
 minim_inter_gradio = gr.ChatInterface(
     fn=respond,
-    inputs=gr.Textbox(label="Your Name"),
-    outputs="text",
     title="Chatbot Minimal",
-    description="Un exemple simple de chatbot avec Gradio"
+    description="Un exemple simple de chatbot avec Gradio",
+    chatbot=gr.Chatbot(type="messages")  
 )
 
 # Lancement de l'interface
